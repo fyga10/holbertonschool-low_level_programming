655b05fc635844f8a3cdb3f54f0850675130b279
fyga10 <frankgiraldo62@outlook.com>
2021-10-04 23:50:53 +0000

    t3a2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,18 @@
  */
 int main(void)
 {
-  int a = 97;
-
-  while (a < 123)
-    {
-      putchar(a);
-      a++;
-    }
-  putchar('\n');
-  return (0);
+int a = 97;
+int b = 65;
+while (a < 123)
+{
+putchar(a);
+a++;
+}
+while (b < 90)
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+return (0);
 }
