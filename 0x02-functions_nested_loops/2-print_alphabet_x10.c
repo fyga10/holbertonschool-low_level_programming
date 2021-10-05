37f48f38a867ace9ba801c91d2623aee71610ad4
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 14:21:37 +0000

    t2a3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
 void print_alphabet_x10(void)
 {
 int i;
- int a = 97;
+int a = 97;
 for (i = 0; i <= 9; i++)
 {
-while (a<123)
+while (a < 123)
 {
 _putchar(a); 
 a++;
 }
-_putchar('\n');
 }
+_putchar('\n');
 }
