a35480672fa759553edc6770e9f3e12d73d20858
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 01:12:52 +0000

    t7a0

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+/**
+ * main - laphabet reverse
+ *
+ *Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+int a=122;
+while (a > 60)
+{
+putchar(a);
+a--;
+}
+putchar('\n');
+return(0);
+}
