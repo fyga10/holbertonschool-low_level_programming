6be3df0e26e85749deb0c830b44fcae1444b0832
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 00:01:59 +0000

    t4a0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+ int b = 97;
+ while (b < 123)
+ {
+ if ((b != 101) && (b != 113))
+ {
+ putchar(b);
+ }
+ b++;
+ }
+ putchar('\n');
+  return (0);
+}
