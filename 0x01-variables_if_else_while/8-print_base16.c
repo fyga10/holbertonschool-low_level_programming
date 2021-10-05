e62ddc81ea9ca073b1bf29a54e06b7cc9c6de337
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 02:50:01 +0000

    t8a0

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+
+/**
+ *
+ *main - hexadecimal
+ *
+ *Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+int a;
+int b;
+
+for (a = '0'; a <= '9', a++)
+{
+putchar(a);
+}
+for (b = 97, b <= 102, b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
+}
