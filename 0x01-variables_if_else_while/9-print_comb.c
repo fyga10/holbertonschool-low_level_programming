768c8985dbc3a97989f9c41600750be365d7e922
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 03:04:39 +0000

    t9a0

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+/**
+ * main - combinations
+ *
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+putchar(a,",",' ');
+putchar('\n');
+return (0);
+}
