bf23d5463ceb1f2c11de432d99bae3d2631611a9
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 22:06:24 +0000

    t6a0

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_abs(int) - absolute value
+ *
+ *Return: always 0
+ */
+
+int _abs(int)
+{
+int r;
+int c;
+if (r < 0)
+{
+c = (r*(-1));
+_putchar('c');
+}
+else (r >= 0)
+{
+c= r;
+_putchar('c');
+}
+return(c);
+}
