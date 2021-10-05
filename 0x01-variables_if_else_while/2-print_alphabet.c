66c7ab4d427522daa33f29f9f0d87db9ea255439
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 00:14:24 +0000

    t2a4

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,14 @@
  *
  * return: always 0 (success)
  */
-
 int main(void)
-
 {
 int c = 97;
- while (c <= 122)
-   {
-     putchar(c);
-     c++;
-   }
- putchar('\n');
-   return (0);
+while (c <= 122)
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
