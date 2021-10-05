3c0be398213ccdef745a8eb4a0d1d72e8ade4b7f
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 00:09:08 +0000

    t2a0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+
+/**
+ * main - prints alphabet
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+
+{
+int c = 97;
+ while (a <= 122)
+   {
+     putchar(a);
+     a++;
+   }
+ putchar('\n')
+   return (0);
+}
