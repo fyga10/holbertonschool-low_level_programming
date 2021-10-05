8309c944bea14378cd45607fd64e5b860f0d694d
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 00:04:28 +0000

    t4a1

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
- int b = 97;
- while (b < 123)
- {
- if ((b != 101) && (b != 113))
- {
- putchar(b);
- }
- b++;
- }
- putchar('\n');
-  return (0);
+int b = 97;
+while (b < 123)
+{
+if ((b != 101) && (b != 113))
+{
+putchar(b);
+}
+b++;
+}
+putchar('\n');
+return (0);
 }
