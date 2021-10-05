a9f4c6d95d79ce477de92aafba72c55a2c5e4263
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 15:47:22 +0000

    t3a0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_islower - check lowercase
+ *
+ *Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+int a;
+for (a = 97; a <123; a++)
+if ( a = 'c');
+{
+return(1);
+else 
+{
+return (0);
+}   
+}
+}
