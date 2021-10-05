d711245e9c0c20ac839953e9bca748a4ccbad77b
fyga10 <frankgiraldo62@outlook.com>
2021-10-05 00:25:59 +0000

    t5a0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ *Return: always 0 (success)
+ */
+int main (void)
+{
+int a = 0;
+while (a < 10)
+{
+printf(a);
+a++;
+}
+printf('\n');
+return (0);
+}
