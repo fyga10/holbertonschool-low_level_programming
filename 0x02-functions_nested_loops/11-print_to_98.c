e7b033117aff8f395693ccf6d8dd0499ef33eef8
fyga10 <frankgiraldo62@outlook.com>
2021-10-06 03:00:30 +0000

    t11a3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,22 +16,22 @@ _putchar(x);
 }
 else if (x < 98)
 {
-for (a == x; a <= 98; a++)
+for (a = x; a <= 98; a++)
 {
 _putchar(48 + (a / 10));
 _putchar(48 + (a % 10));
 _putchar(44);
 _putchar(32);
 }
+}
 else
 {
-for(b == x; b >= 98; b--)
+for(b = x; b >= 98; b--)
 {
 _putchar(48 + (b / 10));
 _putchar(48 + (b % 10));
 _putchar(44);
 _putchar(32);
-}
 }
 }
 return (0);
