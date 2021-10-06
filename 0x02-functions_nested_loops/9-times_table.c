5386f5775b33b6af42acdd298160af7aa6bcee7a
fyga10 <frankgiraldo62@outlook.com>
2021-10-06 01:37:43 +0000

    t9a4

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,10 +14,24 @@ for (i = 0; i <= 9; i++)
 for (x = 0; x <= 9; x++)
 {
 y = (x * i);
-_putchar(y + 48);
+if(y < 10)
+{
+if(y < 0)
+{
 _putchar(44);
 _putchar(32);
+_putchar(32);
 }
-_putchar(10);
+_putchar(y + 48);
+}
+else
+{
+_putchar(44);
+_putchar(32);
+_putchar('0' + (y / 10));
+_putchar('0' + (y % 10));
+}
+}
+_putchar(10)
 }
 }
