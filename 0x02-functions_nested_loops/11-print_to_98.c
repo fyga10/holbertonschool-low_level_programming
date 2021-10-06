8e0a12116038095e1018dca1e084716031025abd
fyga10 <frankgiraldo62@outlook.com>
2021-10-06 02:56:10 +0000

    t11a0

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - natural numbers
+ * @x: Check
+ *
+ * Return: Void
+ */
+void print_to_98(int x)
+{
+  int a, b;
+if (x == 98)
+{
+_putchar(x);
+}
+else if (x < 98)
+{
+for (a == x; a <= 98; a++)
+{
+_putchar(48 + (a / 10));
+_putchar(48 + (a % 10));
+_putchar(44);
+_putchar(32);
+}
+else
+{
+for(b == x; b >= 98; b--)
+{
+_putchar(48 + (b / 10));
+_putchar(48 + (b % 10));
+_putchar(44);
+_putchar(32);
+}
+}
+}
+return (0);
+}
