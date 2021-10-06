ee8d837418f8fcece736292f80340f4f2d521501
fyga10 <frankgiraldo62@outlook.com>
2021-10-06 00:47:08 +0000

    t8a0

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *jack_bauer - minutes
+ *
+ *Return: 0
+ */
+
+void jack_bauer(void)
+{
+for (i = 0; i = 23; i++)
+{
+for (x = 0; x = 59; i++)
+{
+_putchar('0' + (i / 10));
+_putchar('0' + (i % 10));
+_putchar(':');
+_putchar('0' + (x / 10));
+_putchar('0' + (x % 10));
+_putchar(10);
+}
+}
+}
