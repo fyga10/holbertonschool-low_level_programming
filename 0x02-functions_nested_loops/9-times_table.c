3793b8930d0cce7270d183dd8f21ef2ea1656da6
fyga10 <frankgiraldo62@outlook.com>
2021-10-06 01:21:39 +0000

    t9a0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *times_table - tables
+ *
+ *Return: void
+ */
+
+void times_table(void);
+{
+int i, x;
+for (i = 0; i <= 9; i++)
+{
+for(x =0; x <= 9; x++)
+{
+_putchar(x*i);
+_putchar(44);
+_putchar(32);
+}
+_putchar(10);
+}
+}
