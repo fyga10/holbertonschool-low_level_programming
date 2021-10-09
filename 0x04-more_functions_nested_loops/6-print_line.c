3dd4a11b62df83b0aa9caafcb5d91cb522e2472f
fyga10 <frankgiraldo62@outlook.com>
2021-10-09 03:28:22 +0000

    t6a1

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,10 @@ void print_line(int n)
 int x;
 if (n > 0)
 {
-for (x = 0; x <= n; x++)
+for (x = 0; x < n; x++)
 {
 _putchar (95);
 }
-_putchar(10);
 }
-else if (n <= 0)
-{
-_putchar(10);
-}
+_putchar (10);
 }
