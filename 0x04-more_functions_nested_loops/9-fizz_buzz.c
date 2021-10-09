b2563a393b0c31bd95f38639e948264555e95408
fyga10 <frankgiraldo62@outlook.com>
2021-10-09 05:42:28 +0000

    t9a0

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz
+ *
+ *Return: 0 
+ */
+
+int main(void)
+{
+int x;
+for (x = 1; x <= 100; x++)
+{
+if (x % 3 == 0 && x % 15 !=0)
+{
+printf("Fizz " );
+}
+else if (x % 5 == 0 && x % 15 !=0)
+{
+printf("Buzz " );
+}
+else if (x % 15 == 0)
+{
+printf("FizzBuzz ");
+}
+else 
+{
+printf("%d ", x);
+}
+}
+printf("\n");
+return (0);
+}
