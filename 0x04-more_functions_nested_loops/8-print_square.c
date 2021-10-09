823a05ccdbcb10c2dbf2c5b61651a8ce9d2b5ea2
fyga10 <frankgiraldo62@outlook.com>
2021-10-09 04:38:35 +0000

    t8a0

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - square
+ * @size: size
+ */
+
+void print_square(int size)
+{
+int a, b;
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+_putchar (35);
+for (b = 1; b < size; b++)
+{
+_putchar (35);
+}
+_putchar (10);
+}
+}
+_putchar (10);
+}
