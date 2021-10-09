7222f170625b5bb47616e706fd6d361cf708d3fd
fyga10 <frankgiraldo62@outlook.com>
2021-10-09 03:05:50 +0000

    t5a0

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - numbers
+ *
+ */
+
+void more_numbers(void)
+{
+  int c, x;
+for (x = 0; x <=10; x++)
+{
+for(c = 0; c <= 14; c++)
+{
+if (c >= 10)
+{
+_putchar (48 + c / 10);
+}
+_putchar(48 + c % 10);
+}
+_putchar(10);
+}
+}
