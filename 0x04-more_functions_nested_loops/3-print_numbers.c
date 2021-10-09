85cbc62bff7408fbe311afc1e10d6d5d3ba4427c
fyga10 <frankgiraldo62@outlook.com>
2021-10-09 02:21:06 +0000

    t3a0

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - numbers
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+_putchar(48 + a);
+_putchar('\n');
+}
