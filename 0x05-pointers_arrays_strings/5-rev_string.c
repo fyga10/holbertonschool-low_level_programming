2427e273a8df9e852ee11b2fa986d724795c3f8e
fyga10 <frankgiraldo62@outlook.com>
2021-10-12 13:18:08 +0000

    t5a1

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse
+ * @s: character
+ * 
+ */
+void rev_string(char *s)
+{
+int x = 0;
+int y = 0;
+int z;
+
+while (s[x] != '\0')
+{
+x++;
+}
+x = x - 1;
+while (y < x)
+{
+z = s[x];
+s[x] = s[y];
+s[y] = z;
+y++;
+x--;
+}
+}
