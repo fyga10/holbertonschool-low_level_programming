77afd81ea22526a279ba4876f8cb174b59e756e5
fyga10 <frankgiraldo62@outlook.com>
2021-10-15 02:02:01 +0000

    t3a0

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * 3-strcmp.c - compare
+ * @s1: pointer 1
+ * @s2: pointer 2
+ * Return: s1-d2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && *s2)
+{
+if (*s1 != *s2)
+{
+return (*s1 - *s2);
+}
+s1++, s2++;
+}
+return (s1-s2);
+}
