1aebbbc1d190c2610dfe0bd7f352f6c9308504cf
fyga10 <frankgiraldo62@outlook.com>
2021-10-15 02:42:29 +0000

    t6a0

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *cap_string - Capitalizes all words of a string
+ *@s: check a string
+ *
+ * Return: Always 0
+ */
+char *cap_string(char *s)
+{
+int b;
+int c;
+char a[13] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\t',
+'\n'};
+for (b = 0; s[b] != '\0'; b++)
+{
+for (c = 0; a[c] != '\0'; c++)
+{
+if (s[0] >= 'a' && s[0] <= 'z')
+{
+s[0] = s[0] - 32;
+}
+if (s[b] == a[c] && s[b + 1] >= 'a' && s[b + 1] <= 'z')
+{
+s[b + 1] = s[b + 1] - 32;
+}
+}
+}
+return (s);
+}
