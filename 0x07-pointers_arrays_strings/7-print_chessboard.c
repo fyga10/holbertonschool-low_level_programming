a5a2b0002a3d42c3f23751728d766de5c61c9c08
fyga10 <frankgiraldo62@outlook.com>
2021-10-18 21:02:10 -0500

    t6a0

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,13 +7,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-int y, z;
-for (y = 0; y < 8; y++)
+int y = 0, z = 0;
+while (y < 8)
 {
 for (z = 0; z < 8; z++)
 {
 _putchar(a[y][z]);
 }
+y++;
 _putchar(10);
 }
 }
