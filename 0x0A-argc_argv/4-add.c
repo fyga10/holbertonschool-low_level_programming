24e40c97c075fcf6a3286071ecd8ce0179ef2101
fyga10 <frankgiraldo62@outlook.com>
2021-10-22 17:48:37 -0500

    t4a0

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add
+ * @argv: arguments
+ * @argc: numbers of arguments
+ * Return: 1
+ */
+
+
+int main(int argc, char *argv[])
+{
+int i, j = 0, sum;
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+if (argv[i][j] < 48 || argv[i][j] > 57)
+{
+printf("Error\n");
+return (1);
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+}
+else
+{
+printf("%d\n", 0);
+}
+return (0);
+}
