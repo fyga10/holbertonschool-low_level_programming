a7dd7f30717d26c7e025780c53a5093f8523e13e
fyga10 <frankgiraldo62@outlook.com>
2021-10-22 16:23:35 -0500

    t2a0

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: counter
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char **argv)
+{
+	for ( ; *argv; argv++)
+		printf("%s\n", *argv);
+	return (0);
+}
