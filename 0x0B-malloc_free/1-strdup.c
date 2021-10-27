71fd30f24e5917bc906fe7be9468a8eecf06e6a5
fyga10 <frankgiraldo62@outlook.com>
2021-10-27 15:44:26 -0500

    t1a0

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - Copies a string
+ * @str: The string
+ * Return: The direction or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *P;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	P = malloc((length(str)) * sizeof(char));
+
+	if (P == NULL)
+		return (NULL);
+
+	for (i = 0; i < length(str); i++)
+		*(P + i) = *(str + i);
+
+	return (P);
+
+}
+
+/**
+ * length - length of a string
+ * @str: string
+ * Return: length
+ */
+
+int length(char *str)
+{
+	if (*(str) == '\0')
+		return (1);
+	else
+		return (1 + length(str + 1));
+
+}
