21e435ff34caf1c478ff2f54c9c4b4f20389391d
fyga10 <frankgiraldo62@outlook.com>
2021-11-01 19:21:01 -0500

    t0a0

---
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - a dog structure
+ * @name: the dogs name
+ * @age: the dogs age
+ * @owner: the owners name
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
