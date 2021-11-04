b6c5b76faf5321fe431a675e50f0ef997a11bd67
fyga10 <frankgiraldo62@outlook.com>
2021-11-04 16:20:50 -0500

    t2a0

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - Print
+ * @separator: Separator
+ * @n: Number strings
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i;
+	char *aux;
+if (n == 0)
+{
+printf("\n");
+return;
+}
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		aux = va_arg(strings, char *);
+		if (aux != NULL)
+			printf("%s", aux);
+		else
+			printf("(nil)");
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+		if (i == n - 1)
+			printf("\n");
+	}
+	va_end(strings);
+}
