59b907fd9bd9eea9e7788f4a460d499acfcb4bcd
fyga10 <frankgiraldo62@outlook.com>
2021-11-03 20:29:17 -0500

    header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef _FUNCTION
 #define _FUNCTION
 #include <stddef.h>
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
