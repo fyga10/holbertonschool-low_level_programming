3c647727fe3e2de1b38ae28db3ed056e3127ebd1
fyga10 <frankgiraldo62@outlook.com>
2021-11-04 14:51:06 -0500

    t0a0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Sums
+ * @n: Number
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list nums;
+	int sum = 0;
+	unsigned int i;
+if (n == 0)
+return (0);
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(nums, int);
+	}
+	va_end(nums);
+	return (sum);
+}
