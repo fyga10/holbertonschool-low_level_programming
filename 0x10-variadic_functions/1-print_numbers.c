c30076f33503abca010766c8daca457025c68ba9
fyga10 <frankgiraldo62@outlook.com>
2021-11-05 08:26:00 -0500

    t1a2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,19 +7,19 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list nums;
-unsigned int i;
+unsigned int z;
 if (n == 0)
 {
 printf("\n");
 return;
 }
 va_start(nums, n);
-for (i = 0; i < n; i++)
+for (z = 0; z < n; z++)
 {
 printf("%d", va_arg(nums, int));
-if (i != n - 1 && separator != NULL)
+if (z != n - 1 && separator != NULL)
 printf("%s", separator);
-if (i == n - 1)
+if (z == n - 1)
 printf("\n");
 }
 va_end(nums);
