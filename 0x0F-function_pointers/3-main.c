840b2598fb3fbb196ecafcd33d9c47399b51595c
fyga10 <frankgiraldo62@outlook.com>
2021-11-09 22:14:16 -0500

    t3a1

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,15 @@
+#include "3-calc.h"
+int (*get_op_func(char *s))(int, int);
+int main(int argc, char *argv[])
+{
+    char *operator = argv[2];
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[3]);
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    get_op_func(operator)(num1, num2);
+    return (0);
+}
