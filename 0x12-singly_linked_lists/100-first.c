b9bc08a5613889d3d6da34094766b288940090cb
fyga10 <frankgiraldo62@outlook.com>
2021-11-11 21:39:04 -0500

    t5a0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void turtle(void) __attribute__ ((constructor));
+
+/**
+ * turtle - check code
+ * Return: 0
+ */
+
+void turtle(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
