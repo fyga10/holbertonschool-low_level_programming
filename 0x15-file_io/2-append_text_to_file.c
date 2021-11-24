b37a96fea18afa388e2be779cc1c393590c2580f
fyga10 <frankgiraldo62@outlook.com>
2021-11-23 20:06:25 -0500

    t2a0

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int length(char *str)
  * append_text_to_file -  appends text at the end of a file
  * @filename: name of the file
  * @text_content: NULL terminated string to add
- * Return: 1 or -1 
+ * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
