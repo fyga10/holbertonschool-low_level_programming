cc126321bbc3e24e07d6f4088188bcc1cd17e8d0
fyga10 <frankgiraldo62@outlook.com>
2021-11-23 20:05:42 -0500

    t2a0

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * length - Measure the length of a string
+ * @str: The string whose length is needed
+ * Return: The length of the string (number of elements)
+ */
+
+int length(char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (0);
+	for (i = 0; *(str + i); i++)
+		;
+
+	return (i);
+}
+/**
+ * append_text_to_file -  appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add
+ * Return: 1 or -1 
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	written = write(fd, text_content, length(text_content));
+	if (written == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
