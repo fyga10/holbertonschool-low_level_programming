3b15b8e1227fddb15d96ed4ac9b3c099f5e10411
fyga10 <frankgiraldo62@outlook.com>
2022-01-06 19:05:16 -0500

    t1a0

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len -  returns the number of elements in a linked list.
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+	if (h == NULL)
+		return (count);
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
